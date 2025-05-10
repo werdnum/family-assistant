10b608b645d2ad6f6d81bf995e0205b108d7a0e8
Andrew Garrett <andrewgarrett@google,com>
2025-05-10 01:06:21 +0000

    Run pylint with multiprocessing to improve performance
    Pylint runs serially by default, which can be slow for larger projects. This change adds the `-j0` flag to the pylint command, enabling it to use all available CPU cores. This significantly reduces the time it takes to run pylint, improving the overall development workflow.

--- scripts/format-and-lint.sh
+++ scripts/format-and-lint.sh
@@ -2,4 +2,4 @@
 
 ${VIRTUAL_ENV:-.venv}/bin/ruff check --fix --ignore=E501 ${@:-src tests} && \
 ${VIRTUAL_ENV:-.venv}/bin/black --preview ${@:-src tests} && \
-${VIRTUAL_ENV:-.venv}/bin/pylint --errors-only ${@:-src tests}
+${VIRTUAL_ENV:-.venv}/bin/pylint -j0 --errors-only ${@:-src tests}
