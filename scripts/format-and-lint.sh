bfd0814867bb98a6b2e4ba1e318dd0c906fe87a8
Andrew Garrett <andrewgarrett@google,com>
2025-05-21 02:40:20 +0000

    update lint script

--- scripts/format-and-lint.sh
+++ scripts/format-and-lint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-${VIRTUAL_ENV:-.venv}/bin/ruff check --fix --ignore=E501 ${@:-src tests} && \
+${VIRTUAL_ENV:-.venv}/bin/ruff check --fix --preview --ignore=E501 ${@:-src tests} && \
 ${VIRTUAL_ENV:-.venv}/bin/ruff format --preview ${@:-src tests} && \
 ${VIRTUAL_ENV:-.venv}/bin/pylint -j0 --errors-only ${@:-src tests}
